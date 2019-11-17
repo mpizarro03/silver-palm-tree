c1a83f447ad49aed07c120415169abfe4709c7a0
MELISSA PIZARRO <mpizarro03@gmail.com>
2019-11-17 16:11:25 -0700

    make buttons bigger, git add image (api down with code 500, added placehoolder img, add button wrapper class and import Button component

--- src/Components/Tile/Tile.js
+++ src/Components/Tile/Tile.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Tile.css";
+import Button from "../Button/Button";
+import image from "../Tile/sn.png";
 
 class Tile extends React.Component {
   constructor(props) {
@@ -9,7 +11,22 @@ class Tile extends React.Component {
     };
   }
   render() {
-    return <div>test</div>;
+    const { data } = this.props;
+    console.log("data", data);
+    return (
+      <div className="tile">
+        test
+        <img className="tile__img" src={image} alt="Tile Art"></img>
+        <div className="tile__buttonWrapper">
+          <Button onClick={() => console.log("play")} type="play" icon="play" />
+          <Button
+            onClick={() => console.log("...")}
+            label="..."
+            type="ellipses"
+          />
+        </div>
+      </div>
+    );
   }
 }
 
